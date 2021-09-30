25d8cbe6fe2a10fee54b85c0ef7624eaa8c7567b
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-09-30 17:35:59 +0700

    Fix command math

--- src/commands/math/index.ts
+++ src/commands/math/index.ts
@@ -7,8 +7,7 @@ const command: Command = {
     name: "math",
     description: "solve math operation",
     async execute(message, args) {
-        const getCommand = message.content.split(' ')
-        const [command, operation, question]: Array<string> = getCommand
+        const [operation, question]: Array<string> = args
         const response = await axios.get(`${newton}/${operation}/${question}`)
         const answer = response.data
         message.reply(`operation : ${answer.operation}\nquestion : ${answer.expression}\n\ranswer : ${answer.result}`)
